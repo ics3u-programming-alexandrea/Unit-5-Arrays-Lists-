e5c06c281b988de78040b771f293b15409564d06
s465640 <54953677+s465640@users.noreply.github.com>
2019-11-27 12:59:23 -0500

    Added FindMaxValue

--- FindMaxValueAlex/FindMaxValueAlex/FindMaxValueForm.Designer.cs
+++ FindMaxValueAlex/FindMaxValueAlex/FindMaxValueForm.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace FindMaxValueAlex
 {
-    partial class Form1
+    partial class frmFindMaxValue
     {
         /// <summary>
         /// Required designer variable.
@@ -28,13 +28,61 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.lstNumbers = new System.Windows.Forms.ListBox();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.lblMaxValue = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lstNumbers
+            // 
+            this.lstNumbers.FormattingEnabled = true;
+            this.lstNumbers.Location = new System.Drawing.Point(207, 81);
+            this.lstNumbers.Name = "lstNumbers";
+            this.lstNumbers.Size = new System.Drawing.Size(246, 199);
+            this.lstNumbers.TabIndex = 0;
+            // 
+            // btnStart
+            // 
+            this.btnStart.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnStart.Location = new System.Drawing.Point(283, 24);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(91, 36);
+            this.btnStart.TabIndex = 1;
+            this.btnStart.Text = "Start";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.BtnStart_Click);
+            // 
+            // lblMaxValue
+            // 
+            this.lblMaxValue.AutoSize = true;
+            this.lblMaxValue.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblMaxValue.Location = new System.Drawing.Point(287, 313);
+            this.lblMaxValue.Name = "lblMaxValue";
+            this.lblMaxValue.Size = new System.Drawing.Size(76, 25);
+            this.lblMaxValue.TabIndex = 2;
+            this.lblMaxValue.Text = "label1";
+            // 
+            // frmFindMaxValue
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(656, 364);
+            this.Controls.Add(this.lblMaxValue);
+            this.Controls.Add(this.btnStart);
+            this.Controls.Add(this.lstNumbers);
+            this.Name = "frmFindMaxValue";
+            this.Text = "Find Max Value by Alex";
+            this.Load += new System.EventHandler(this.FrmFindMaxValue_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.ListBox lstNumbers;
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.Label lblMaxValue;
     }
 }
 
--- FindMaxValueAlex/FindMaxValueAlex/FindMaxValueForm.cs
+++ FindMaxValueAlex/FindMaxValueAlex/FindMaxValueForm.cs
@@ -10,11 +10,30 @@ using System.Windows.Forms;
 
 namespace FindMaxValueAlex
 {
-    public partial class Form1 : Form
+    public partial class frmFindMaxValue : Form
     {
-        public Form1()
+        public frmFindMaxValue()
         {
             InitializeComponent();
+        }
+
+        private void FrmFindMaxValue_Load(object sender, EventArgs e)
+        {
+            lblMaxValue.Hide();
+        }
+
+        private void BtnStart_Click(object sender, EventArgs e)
+        {
+            //declear local variables and constants
+            const int MAX_ARRAY_SIZE = 10;
+            const int MAX_RANDOM_NUMBER = 500;
+            int[] arrayOfIntergers = new int[MAX_ARRAY_SIZE];
+            int counter, randomNumber, maxValue;
+
+            //Function: GetMaxValue
+            //Input: int[] tmpArrayOfIntergers - assume all values 
+            //Output: 
+            //Description:
         }
     }
 }

0e480ab9647869964c5f59e21564e1c4e722b545
s465640 <54953677+s465640@users.noreply.github.com>
2019-12-03 15:20:57 -0500

    Added dealcard

---
+++ DealCardAlex/DealCardAlex/DealCardForm.Designer.cs
@@ -0,0 +1,127 @@
+﻿namespace DealCardAlex
+{
+    partial class frmDealCard
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.picPlayercard1 = new System.Windows.Forms.PictureBox();
+            this.picDealercard1 = new System.Windows.Forms.PictureBox();
+            this.picPlayercard3 = new System.Windows.Forms.PictureBox();
+            this.picPlayercard2 = new System.Windows.Forms.PictureBox();
+            this.picDealercard3 = new System.Windows.Forms.PictureBox();
+            this.picDealercard2 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.picPlayercard1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picDealercard1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picPlayercard3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picPlayercard2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picDealercard3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picDealercard2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // picPlayercard1
+            // 
+            this.picPlayercard1.Location = new System.Drawing.Point(55, 40);
+            this.picPlayercard1.Name = "picPlayercard1";
+            this.picPlayercard1.Size = new System.Drawing.Size(100, 94);
+            this.picPlayercard1.TabIndex = 0;
+            this.picPlayercard1.TabStop = false;
+            // 
+            // picDealercard1
+            // 
+            this.picDealercard1.Location = new System.Drawing.Point(74, 284);
+            this.picDealercard1.Name = "picDealercard1";
+            this.picDealercard1.Size = new System.Drawing.Size(100, 94);
+            this.picDealercard1.TabIndex = 1;
+            this.picDealercard1.TabStop = false;
+            // 
+            // picPlayercard3
+            // 
+            this.picPlayercard3.Location = new System.Drawing.Point(403, 40);
+            this.picPlayercard3.Name = "picPlayercard3";
+            this.picPlayercard3.Size = new System.Drawing.Size(100, 94);
+            this.picPlayercard3.TabIndex = 2;
+            this.picPlayercard3.TabStop = false;
+            // 
+            // picPlayercard2
+            // 
+            this.picPlayercard2.Location = new System.Drawing.Point(243, 40);
+            this.picPlayercard2.Name = "picPlayercard2";
+            this.picPlayercard2.Size = new System.Drawing.Size(100, 94);
+            this.picPlayercard2.TabIndex = 3;
+            this.picPlayercard2.TabStop = false;
+            // 
+            // picDealercard3
+            // 
+            this.picDealercard3.Location = new System.Drawing.Point(403, 284);
+            this.picDealercard3.Name = "picDealercard3";
+            this.picDealercard3.Size = new System.Drawing.Size(100, 94);
+            this.picDealercard3.TabIndex = 4;
+            this.picDealercard3.TabStop = false;
+            // 
+            // picDealercard2
+            // 
+            this.picDealercard2.Location = new System.Drawing.Point(229, 284);
+            this.picDealercard2.Name = "picDealercard2";
+            this.picDealercard2.Size = new System.Drawing.Size(100, 94);
+            this.picDealercard2.TabIndex = 5;
+            this.picDealercard2.TabStop = false;
+            // 
+            // frmDealCard
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(711, 455);
+            this.Controls.Add(this.picDealercard2);
+            this.Controls.Add(this.picDealercard3);
+            this.Controls.Add(this.picPlayercard2);
+            this.Controls.Add(this.picPlayercard3);
+            this.Controls.Add(this.picDealercard1);
+            this.Controls.Add(this.picPlayercard1);
+            this.Name = "frmDealCard";
+            this.Text = "Deal Card By Alex";
+            this.Load += new System.EventHandler(this.FrmDealCard_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.picPlayercard1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picDealercard1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picPlayercard3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picPlayercard2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picDealercard3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picDealercard2)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox picPlayercard1;
+        private System.Windows.Forms.PictureBox picDealercard1;
+        private System.Windows.Forms.PictureBox picPlayercard3;
+        private System.Windows.Forms.PictureBox picPlayercard2;
+        private System.Windows.Forms.PictureBox picDealercard3;
+        private System.Windows.Forms.PictureBox picDealercard2;
+    }
+}
+

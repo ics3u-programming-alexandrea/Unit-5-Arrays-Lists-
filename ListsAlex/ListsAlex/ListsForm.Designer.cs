a36015f9e66d251fb21f1c087db577f347aa7197
s465640 <54953677+s465640@users.noreply.github.com>
2019-12-03 13:10:22 -0500

    Added lists

---
+++ ListsAlex/ListsAlex/ListsForm.Designer.cs
@@ -0,0 +1,126 @@
+﻿namespace ListsAlex
+{
+    partial class frmLists
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblEnter = new System.Windows.Forms.Label();
+            this.btnEnter = new System.Windows.Forms.Button();
+            this.lstMarks = new System.Windows.Forms.ListBox();
+            this.txtMark = new System.Windows.Forms.TextBox();
+            this.btnCalculate = new System.Windows.Forms.Button();
+            this.lblAverage = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lblEnter
+            // 
+            this.lblEnter.AutoSize = true;
+            this.lblEnter.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblEnter.Location = new System.Drawing.Point(73, 35);
+            this.lblEnter.Name = "lblEnter";
+            this.lblEnter.Size = new System.Drawing.Size(275, 20);
+            this.lblEnter.TabIndex = 0;
+            this.lblEnter.Text = "Enter a mark between 0 and 100:";
+            // 
+            // btnEnter
+            // 
+            this.btnEnter.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnEnter.Location = new System.Drawing.Point(409, 80);
+            this.btnEnter.Name = "btnEnter";
+            this.btnEnter.Size = new System.Drawing.Size(100, 30);
+            this.btnEnter.TabIndex = 1;
+            this.btnEnter.Text = "Enter";
+            this.btnEnter.UseVisualStyleBackColor = true;
+            this.btnEnter.Click += new System.EventHandler(this.BtnEnter_Click);
+            // 
+            // lstMarks
+            // 
+            this.lstMarks.FormattingEnabled = true;
+            this.lstMarks.Location = new System.Drawing.Point(131, 116);
+            this.lstMarks.Name = "lstMarks";
+            this.lstMarks.Size = new System.Drawing.Size(313, 225);
+            this.lstMarks.TabIndex = 2;
+            // 
+            // txtMark
+            // 
+            this.txtMark.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtMark.Location = new System.Drawing.Point(409, 35);
+            this.txtMark.Name = "txtMark";
+            this.txtMark.Size = new System.Drawing.Size(100, 26);
+            this.txtMark.TabIndex = 3;
+            // 
+            // btnCalculate
+            // 
+            this.btnCalculate.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCalculate.Location = new System.Drawing.Point(203, 357);
+            this.btnCalculate.Name = "btnCalculate";
+            this.btnCalculate.Size = new System.Drawing.Size(196, 30);
+            this.btnCalculate.TabIndex = 4;
+            this.btnCalculate.Text = "Calculate Average";
+            this.btnCalculate.UseVisualStyleBackColor = true;
+         
+            // 
+            // lblAverage
+            // 
+            this.lblAverage.AutoSize = true;
+            this.lblAverage.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblAverage.Location = new System.Drawing.Point(251, 411);
+            this.lblAverage.Name = "lblAverage";
+            this.lblAverage.Size = new System.Drawing.Size(75, 20);
+            this.lblAverage.TabIndex = 5;
+            this.lblAverage.Text = "Average";
+            // 
+            // frmLists
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lblAverage);
+            this.Controls.Add(this.btnCalculate);
+            this.Controls.Add(this.txtMark);
+            this.Controls.Add(this.lstMarks);
+            this.Controls.Add(this.btnEnter);
+            this.Controls.Add(this.lblEnter);
+            this.Name = "frmLists";
+            this.Text = "Lists by Alex";
+            this.Load += new System.EventHandler(this.FrmLists_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblEnter;
+        private System.Windows.Forms.Button btnEnter;
+        private System.Windows.Forms.ListBox lstMarks;
+        private System.Windows.Forms.TextBox txtMark;
+        private System.Windows.Forms.Button btnCalculate;
+        private System.Windows.Forms.Label lblAverage;
+    }
+}
+
---
+++ ListsAlex/ListsAlex/ListsForm.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ListsAlex
+{
+    public partial class frmLists : Form
+    {
+        List<int> myListOfIntegers;
+        public frmLists()
+        {
+            InitializeComponent();
+        }
+
+        private void FrmLists_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void BtnEnter_Click(object sender, EventArgs e)
+        {
+            //declare local variables
+            int userMark = -1;
+            //convert the value in the textbox to an integer
+            try
+            {
+                userMark = int.Parse(txtMark.Text);
+            }
+            //if the user did not enter a valid number, write the error to the command line
+            catch(Exception parseError)
+            {
+                Console.WriteLine("An error occured:  '{0}'", parseError);
+            }
+            //check if the user entered a number between 0 and 100
+            if ((userMark >= 0) && (userMark<= 100) )
+            {
+                //add the mark to the listbox 
+                this.lstMarks.Items.Add(userMark);
+                //add the mark to the list
+                myListOfIntegers.Add(userMark);
+            }
+            //otherwise, display an error
+            else
+            {
+                MessageBox.Show("Please enter a number between 0 and 100.", "Invalid Number");
+            }
+        }
+
+       
+    }
+}

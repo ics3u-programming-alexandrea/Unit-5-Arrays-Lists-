5c1efe96846ad903fc850b6891da5b7f75430b42
s465640 <54953677+s465640@users.noreply.github.com>
2019-12-05 15:03:18 -0500

    Added Deal Card

--- DealCardAlex/DealCardAlex/DealCardForm.cs
+++ DealCardAlex/DealCardAlex/DealCardForm.cs
@@ -1,4 +1,12 @@
-﻿using System;
+﻿
+/*
+ * Created by: Alex Ude
+ * Created on: 05-December-2019
+ * Created for: ICS3U Programming
+ * Daily Assignment – Day #40 - Deal Card
+ * This program changes the card in a picture box when it is clicked
+*/
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -32,51 +40,69 @@ namespace DealCardAlex
         //Description:  this procedure populates the deck of cards and the values
         private void CreateDeck ()
         {
+            ListCardImages.Clear();
+            ListCardValues.Clear();
             //Add all cards to a list
            ListCardImages.Add(Properties.Resources.AC);
            ListCardImages.Add(Properties.Resources.AD);
            ListCardImages.Add(Properties.Resources.AH);
            ListCardImages.Add(Properties.Resources.AS);
+
            ListCardImages.Add(Properties.Resources._2C);
            ListCardImages.Add(Properties.Resources._2D);
            ListCardImages.Add(Properties.Resources._2H);
            ListCardImages.Add(Properties.Resources._2S);
+
            ListCardImages.Add(Properties.Resources._3C);
            ListCardImages.Add(Properties.Resources._3D);
            ListCardImages.Add(Properties.Resources._3H);
            ListCardImages.Add(Properties.Resources._3S);
+
            ListCardImages.Add(Properties.Resources._4C);
            ListCardImages.Add(Properties.Resources._4D);
            ListCardImages.Add(Properties.Resources._4H);
            ListCardImages.Add(Properties.Resources._4S);
+
            ListCardImages.Add(Properties.Resources._5C);
            ListCardImages.Add(Properties.Resources._5D);
            ListCardImages.Add(Properties.Resources._5H);
            ListCardImages.Add(Properties.Resources._5S);
+
            ListCardImages.Add(Properties.Resources._6C);
            ListCardImages.Add(Properties.Resources._6D);
            ListCardImages.Add(Properties.Resources._6H);
            ListCardImages.Add(Properties.Resources._6S);
+
            ListCardImages.Add(Properties.Resources._7C);
            ListCardImages.Add(Properties.Resources._7D);
            ListCardImages.Add(Properties.Resources._7H);
            ListCardImages.Add(Properties.Resources._7S);
+
            ListCardImages.Add(Properties.Resources._8C);
            ListCardImages.Add(Properties.Resources._8D);
            ListCardImages.Add(Properties.Resources._8H);
            ListCardImages.Add(Properties.Resources._8S);
+
            ListCardImages.Add(Properties.Resources._9C);
            ListCardImages.Add(Properties.Resources._9D);
            ListCardImages.Add(Properties.Resources._9H);
            ListCardImages.Add(Properties.Resources._9S);
-           ListCardImages.Add(Properties.Resources.JC);
+
+            ListCardImages.Add(Properties.Resources._10C);
+            ListCardImages.Add(Properties.Resources._10D);
+            ListCardImages.Add(Properties.Resources._10H);
+            ListCardImages.Add(Properties.Resources._10S);
+
+            ListCardImages.Add(Properties.Resources.JC);
            ListCardImages.Add(Properties.Resources.JD);
            ListCardImages.Add(Properties.Resources.JS);
            ListCardImages.Add(Properties.Resources.JH);
+
            ListCardImages.Add(Properties.Resources.QC);
            ListCardImages.Add(Properties.Resources.QD);
            ListCardImages.Add(Properties.Resources.QH);
            ListCardImages.Add(Properties.Resources.QS);
+
            ListCardImages.Add(Properties.Resources.KC);
            ListCardImages.Add(Properties.Resources.KD);
            ListCardImages.Add(Properties.Resources.KH);
@@ -87,56 +113,72 @@ namespace DealCardAlex
            ListCardValues.Add(1);
            ListCardValues.Add(1);
            ListCardValues.Add(1);
+
            ListCardValues.Add(2);
            ListCardValues.Add(2);
            ListCardValues.Add(2);
            ListCardValues.Add(2);
+
            ListCardValues.Add(3);
            ListCardValues.Add(3);
            ListCardValues.Add(3);
            ListCardValues.Add(3);
+
            ListCardValues.Add(4);
            ListCardValues.Add(4);
            ListCardValues.Add(4);
            ListCardValues.Add(4);
+
            ListCardValues.Add(5);
            ListCardValues.Add(5);
            ListCardValues.Add(5);
            ListCardValues.Add(5);
+
            ListCardValues.Add(6);
            ListCardValues.Add(6);
            ListCardValues.Add(6);
            ListCardValues.Add(6);
+
            ListCardValues.Add(7);
            ListCardValues.Add(7);
            ListCardValues.Add(7);
            ListCardValues.Add(7);
+
            ListCardValues.Add(8);
            ListCardValues.Add(8);
            ListCardValues.Add(8);
            ListCardValues.Add(8);
+
            ListCardValues.Add(9);
            ListCardValues.Add(9);
            ListCardValues.Add(9);
            ListCardValues.Add(9);
-           ListCardValues.Add(10);
-           ListCardValues.Add(10);
-           ListCardValues.Add(10);
-           ListCardValues.Add(10);
-           ListCardValues.Add(10);
-           ListCardValues.Add(10);
-           ListCardValues.Add(10);
-           ListCardValues.Add(10);
+
            ListCardValues.Add(10);
            ListCardValues.Add(10);
            ListCardValues.Add(10);
            ListCardValues.Add(10);
 
+           ListCardValues.Add(10);
+           ListCardValues.Add(10);
+           ListCardValues.Add(10);
+           ListCardValues.Add(10);
+
+           ListCardValues.Add(10);
+           ListCardValues.Add(10);
+           ListCardValues.Add(10);
+           ListCardValues.Add(10);
+
+            ListCardValues.Add(10);
+            ListCardValues.Add(10);
+            ListCardValues.Add(10);
+            ListCardValues.Add(10);
+
             //procedure: DealCard
             //input: ref picturebox aPictureBox, int randomIndex
             //output: int
             //Description:  this function deals the cards to each picture box
- 
+
         }
         private int DealCard(ref PictureBox aPictureBox, int randomIndex)
         {
@@ -151,10 +193,12 @@ namespace DealCardAlex
 
             //remove the image from the list of cards
             ListCardImages.RemoveAt(randomIndex);
+            Console.WriteLine("***List of card images count = " + ListCardImages.Count());
 
             //get the value of the card
             Value = ListCardValues[randomIndex];
             ListCardValues.RemoveAt (randomIndex);
+            Console.WriteLine("***List of card values count = " + ListCardValues.Count());
             return Value;
             
         }
@@ -235,6 +279,7 @@ namespace DealCardAlex
             if (ListCardImages.Count() == 0)
             {
                 CreateDeck();
+                Console.WriteLine("***Created new deck.");
             }
             else
             {
---
+++ FileRWAlex/FileRWAlex/Form1.Designer.cs
@@ -0,0 +1,89 @@
+﻿namespace FileRWAlex
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnCheck = new System.Windows.Forms.Button();
+            this.lblCompare = new System.Windows.Forms.Label();
+            this.lblCases = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnCheck
+            // 
+            this.btnCheck.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCheck.Location = new System.Drawing.Point(193, 157);
+            this.btnCheck.Name = "btnCheck";
+            this.btnCheck.Size = new System.Drawing.Size(104, 40);
+            this.btnCheck.TabIndex = 0;
+            this.btnCheck.Text = "Check";
+            this.btnCheck.UseVisualStyleBackColor = true;
+            this.btnCheck.Click += new System.EventHandler(this.BtnCheck_Click);
+            // 
+            // lblCompare
+            // 
+            this.lblCompare.AutoSize = true;
+            this.lblCompare.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblCompare.Location = new System.Drawing.Point(56, 9);
+            this.lblCompare.Name = "lblCompare";
+            this.lblCompare.Size = new System.Drawing.Size(485, 25);
+            this.lblCompare.TabIndex = 1;
+            this.lblCompare.Text = "Compare strings on each line of input.txt. file";
+            // 
+            // lblCases
+            // 
+            this.lblCases.AutoSize = true;
+            this.lblCases.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblCases.Location = new System.Drawing.Point(200, 46);
+            this.lblCases.Name = "lblCases";
+            this.lblCases.Size = new System.Drawing.Size(228, 25);
+            this.lblCases.TabIndex = 2;
+            this.lblCases.Text = "(Cases doesn\'t matter)";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(553, 295);
+            this.Controls.Add(this.lblCases);
+            this.Controls.Add(this.lblCompare);
+            this.Controls.Add(this.btnCheck);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnCheck;
+        private System.Windows.Forms.Label lblCompare;
+        private System.Windows.Forms.Label lblCases;
+    }
+}
+
